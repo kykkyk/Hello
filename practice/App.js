2879820ad662932f4341afe5f71b5b37a60d1bdb
kykkyk <kyg9855@naver.com>
2021-09-07 00:28:42 +0900

    test

---
+++ practice/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {StyleSheet, Text, View, Button, Touchopacity} from 'react-native';
+import StackPage from './stack';
+
+const App = ({navigation}) => {
+  return <StackPage />;
+};
+
+export default App;
---
+++ practice/stack.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {StyleSheet, Text, view} from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
+import { NavigatorContainer } from '@react-navigation/native';
+import 'react-native-gesture-handler';
+
+ 
